0601d8f3aa89da6a9ccdfd1af0d9d12ec95a362d
hachiwarenation <nemomartin11@gmail.com>
2025-10-23 13:05:13 +0100

    Implement PID controller instead of PD

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -3,19 +3,26 @@ import numpy as np
 
 class Controller:
     def __init__(self):
-        self.K_P = 0.07
-        self.K_D = 0.8
-        self.e_t = 0
-        self.e_t1 = False
+        self.K_P = 0.06
+        self.K_I = 0.007
+        self.K_D = 0.9
+        self.dt = 1
+        # Aggregate constants for concise use & fewer calculations
+        self.A = self.K_P + self.K_I*self.dt + self.K_D/self.dt
+        self.B = -self.K_P - 2*self.K_D/self.dt
+        self.C = self.K_D/self.dt
+        # corresponding ordering in maths: e[t], e[t-1], e[t-2]
+        self.e = [0,0,0]
+        self.u = [0,0]
+
 
     def run_controller(self, reference_t, observation_t):
-        self.e_t = reference_t - observation_t
-        #At the very start, assume zero derivative
-        if not self.e_t1:
-            self.e_t1 = self.e_t
-        u = self.K_P * self.e_t + self.K_D * (self.e_t - self.e_t1)
-        self.e_t1 = self.e_t
-        return u
+        self.e[0] = reference_t - observation_t
+        self.u[0] = self.u[1] + self.A * self.e[0] + self.B * self.e[1] + self.C * self.e[2]
+        self.e[2] = self.e[1]
+        self.e[1] = self.e[0]
+        self.u[1] = self.u[0]
+        return self.u[0]
     
     def reset_controller(self):
         self.__init__()
